d52cfc736367bbce2efc88288869f6b9a75dc3d4
Josh Tumath <josh@joshtumath.uk>
2016-01-27 23:06:07 +0000

    Added HTML and script for toggling nav menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,9 +15,9 @@
 
         $(".scroll-down").arctic_scroll();
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+        $("#nav-toggle").on("click", function (e) {
             e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
+            $("#nav").toggleClass("open");
         });
 
     });
